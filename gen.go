112c1db511043645482e961c8002a160dc490390
Dinko Korunic <dinko.korunic@gmail.com>
2025-07-31 10:00:50 +0200

    Move eBPF code to bpf/ folder.

--- gen.go
+++ gen.go
@@ -21,8 +21,8 @@
 
 package main
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 counter counter.c -- -I./contrib/amd64
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 counter counter.c -- -I./contrib/arm64
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 counter ./bpf/counter.c -- -I./contrib/amd64
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 counter ./bpf/counter.c -- -I./contrib/arm64
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 cgroup cgroup.c -- -I./contrib/amd64
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 cgroup cgroup.c -- -I./contrib/arm64
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 cgroup ./bpf/cgroup.c -- -I./contrib/amd64
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 cgroup ./bpf/cgroup.c -- -I./contrib/arm64
