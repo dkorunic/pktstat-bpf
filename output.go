26711f7ad7cc757f0d11c9d38670bae616000f4c
Dinko Korunic <dinko.korunic@gmail.com>
2025-08-12 20:47:27 +0200

    Add eBPF map read retrying

--- output.go
+++ output.go
@@ -28,6 +28,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/avast/retry-go/v4"
 	"github.com/cilium/ebpf"
 	"github.com/goccy/go-json"
 )
@@ -38,6 +39,8 @@ const (
 	Mbps         = 1000 * Kbps
 	Gbps         = 1000 * Mbps
 	Tbps         = 1000 * Gbps
+
+	mapReadRetries = 3
 )
 
 // processMap generates statEntry objects from an ebpf.Map using the provided start time.
@@ -53,28 +56,41 @@ func processMap(m *ebpf.Map, start time.Time, sortFunc func([]statEntry)) ([]sta
 
 	dur := time.Since(start).Seconds()
 	stats := make([]statEntry, 0, m.MaxEntries())
-	iter := m.Iterate()
 
-	// build statEntry slice converting data where needed
-	for iter.Next(&key, &val) {
-		stats = append(stats, statEntry{
-			SrcIP:   bytesToAddr(key.Srcip.In6U.U6Addr8),
-			DstIP:   bytesToAddr(key.Dstip.In6U.U6Addr8),
-			Proto:   protoToString(key.Proto),
-			SrcPort: key.SrcPort,
-			DstPort: key.DstPort,
-			Bytes:   val.Bytes,
-			Packets: val.Packets,
-			Bitrate: 8 * float64(val.Bytes) / dur,
-			Pid:     key.Pid,
-			Comm:    bsliceToString(key.Comm[:]),
-			CGroup:  cGroupToPath(key.Cgroupid),
-		})
+	err := retry.Do(func() error {
+		// reset to zero length
+		stats = stats[:0]
+
+		iter := m.Iterate()
+
+		// build statEntry slice converting data where needed
+		for iter.Next(&key, &val) {
+			stats = append(stats, statEntry{
+				SrcIP:   bytesToAddr(key.Srcip.In6U.U6Addr8),
+				DstIP:   bytesToAddr(key.Dstip.In6U.U6Addr8),
+				Proto:   protoToString(key.Proto),
+				SrcPort: key.SrcPort,
+				DstPort: key.DstPort,
+				Bytes:   val.Bytes,
+				Packets: val.Packets,
+				Bitrate: 8 * float64(val.Bytes) / dur,
+				Pid:     key.Pid,
+				Comm:    bsliceToString(key.Comm[:]),
+				CGroup:  cGroupToPath(key.Cgroupid),
+			})
+		}
+
+		return iter.Err()
+	},
+		retry.Attempts(mapReadRetries),
+	)
+	if err != nil {
+		return nil, err
 	}
 
 	sortFunc(stats)
 
-	return stats, iter.Err()
+	return stats, nil
 }
 
 // bitrateSort sorts a slice of statEntry objects by their Bitrate field in descending order.
