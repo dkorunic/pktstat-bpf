5f66c535dfcb9a88f43a819c3e30d2e8f200e8e8
Dinko Korunic <dinko.korunic@gmail.com>
2026-03-12 10:21:36 +0100

    Fix formatting.

--- bpf/counter.h
+++ bpf/counter.h
@@ -42,7 +42,7 @@
 #define MAX_ENTRIES 131072
 
 // IPv6 extension header protocol numbers
-#define IPPROTO_HOPOPTS  0  // Hop-by-Hop Options
-#define IPPROTO_ROUTING  43 // Routing Header
+#define IPPROTO_HOPOPTS 0   // Hop-by-Hop Options
+#define IPPROTO_ROUTING 43  // Routing Header
 #define IPPROTO_FRAGMENT 44 // Fragment Header
-#define IPPROTO_DSTOPTS  60 // Destination Options
+#define IPPROTO_DSTOPTS 60  // Destination Options
