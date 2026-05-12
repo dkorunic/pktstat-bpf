8dce86b1a504c14d5970bc1864ae0024b1c30e62
Dinko Korunic <dkorunic@haproxy.com>
2026-05-12 16:20:07 +0000

    Add AppProto field to statEntry

--- types.go
+++ types.go
@@ -28,17 +28,18 @@ import (
 )
 
 type statEntry struct {
-	SrcIP   netip.Addr `json:"srcIp"`
-	DstIP   netip.Addr `json:"dstIp"`
-	Proto   string     `json:"proto"`
-	Comm    string     `json:"comm,omitempty"`
-	CGroup  string     `json:"cgroup,omitempty"`
-	Bytes   uint64     `json:"bytes"`
-	Packets uint64     `json:"packets"`
-	Bitrate float64    `json:"bitrate"`
-	Pid     int32      `json:"pid,omitempty"`
-	SrcPort uint16     `json:"srcPort"`
-	DstPort uint16     `json:"dstPort"`
+	SrcIP    netip.Addr `json:"srcIp"`
+	DstIP    netip.Addr `json:"dstIp"`
+	Proto    string     `json:"proto"`
+	Comm     string     `json:"comm,omitempty"`
+	CGroup   string     `json:"cgroup,omitempty"`
+	AppProto string     `json:"appProto,omitempty"`
+	Bytes    uint64     `json:"bytes"`
+	Packets  uint64     `json:"packets"`
+	Bitrate  float64    `json:"bitrate"`
+	Pid      int32      `json:"pid,omitempty"`
+	SrcPort  uint16     `json:"srcPort"`
+	DstPort  uint16     `json:"dstPort"`
 }
 
 type kprobeHook struct {
